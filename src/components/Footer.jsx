f2017ef65993eb8618faeae7c5fb14b9f5fd060c
priyansh0202-coder <priyanshsoni36@gmail.com>
2024-03-09 19:00:49 +0530

    React Blog App

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Blog from "../img/blog.png"
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Blog} />
+      <span>Made with 💙 and <b>React.js</b>.</span>
+   </footer>
+  )
+}
+
+export default Footer
---
+++ src/pages/Register.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
+const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [err, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((perv) => ({ ...perv, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/auth/register", inputs);
+      navigate("/login");
+    } catch (error) {
+      setError(error.response.data);
+    }
+  };
+
+  return (
+    <div className="auth">
+      <h1>Register</h1>
+      <form>
+        <input
+          required
+          type="text"
+          name="username"
+          placeholder="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="email"
+          name="email"
+          placeholder="email"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Register</button>
+        {err && <p>{err}</p>}
+        <span>
+          Do you have an account? <Link to="/login">Login </Link>
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
